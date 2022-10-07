9e4a2522d6f302af4cb66144a19f030824bc0b82
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2022-10-07 13:28:57 -0400

    write up changed

--- Homework 1/README.md
+++ Homework 1/README.md
@@ -5,6 +5,17 @@ October 1, 2022
 Virtual and Augmented Reality Homework 1: Camera View Transformation
 ```
 
+## Object Position and Visualization
+```
+Object Position in the camera space:
+[ 1.38397467 -0.79903796  0.96410142]
+```
+
+![Output visualization of world space and camera space](images/plot_out.png)
+This is the output of the visualization of where the object is in the world space and camera space.
+
+## General Logic
+
 > Initial camera matrix
 $$\begin{bmatrix} 
 1 & 0 & 0 & 0 \\ 
@@ -37,3 +48,5 @@ $$\begin{bmatrix}
 0 & sin(30) & cos(30) & 0 \\
 0 & 0 & 0 & 1
 \end{bmatrix}$$
+
+Multiply the matrices above to get the composite camera transformation. Invert the composite camera transformation and multiply with the objects world position to get the objects camera position. Then visualize the world space and camera space.
--- Homework 1/hw1.py
+++ Homework 1/hw1.py
@@ -3,22 +3,12 @@ import numpy as np
 import math
 
 # In the beginning, the camera's local coordinate system coincides with the world coordinate system.
-# this is the initial camera transformation matrix
-# [[1. 0. 0. 0.]
-#  [0. 1. 0. 0.]
-#  [0. 0. 1. 0.]
-#  [0. 0. 0. 1.]]
 initial_camera_transformation = np.identity(4, dtype=np.float32)
 
 # First rotate the camera around the z axis by 120 degrees, then move it by a translation of (dx, dy, dz) = (2.0, 3.0, 1.0), finally rotate it around the x axis by 30 degrees.
 # TODO: compute the 4 x 4 matrix corresponding to each transformation described above and composite them into a single transformation matrix, i.e., the camera transformation matrix.
 
 # 1. rotate the camera around the z axis by 120 degrees
-# [[cos(120). -sin(120). 0. 0.]
-#  [sin(120). cos(120).  0. 0.]
-#  [0.          0.       1. 0.]
-#  [0.          0.       0. 1.]]
-
 cosine_120 = math.cos(math.radians(120))
 sine_120 = math.sin(math.radians(120))
 
@@ -27,21 +17,13 @@ rotation1 = np.array([[cosine_120, -sine_120, 0, 0],
                         [0, 0, 1, 0],
                         [0, 0, 0, 1]], dtype=np.float32)
 
-# 2. move it by a translation of (dx, dy, dz) = (2.0, 3.0, 1.0)
-# [[1. 0. 0. 2.]
-#  [0. 1. 0. 3.]
-#  [0. 0. 1. 1.]
-#  [0. 0. 0. 1.]]
+# 2. move camera by a translation of (dx, dy, dz) = (2.0, 3.0, 1.0)
 translation = np.array([[1, 0, 0, 2],
                         [0, 1, 0, 3],
                         [0, 0, 1, 1],
                         [0, 0, 0, 1]], dtype=np.float32)
 
-# 3. rotate it around the x axis by 30 degrees
-# [[1.      0.           0.          0.  ]
-#  [0.      cos(30).  -sin(30).      0.  ]
-#  [0.      sin(30).   cos(30).      0.  ]
-#  [0.      0.           0.          1.  ]]
+# 3. rotate camera around the x axis by 30 degrees
 cosine_30 = math.cos(math.radians(30))
 sine_30 = math.sin(math.radians(30))
 
@@ -53,32 +35,17 @@ rotation2 = np.array([[1, 0, 0, 0],
 # composite them into a single transformation matrix, i.e., the camera transformation matrix.
 current_camera_transformation = np.matmul(np.matmul(rotation2, translation), rotation1)
 
-# Debugs for the camera transformation matrix step 1
-print("Initial camera transformation matrix:")
-print(initial_camera_transformation)
-print("Rotation 120 around z axis:")
-print(rotation1)
-print("Translation (2, 3, 1):")
-print(translation)
-print("Rotation 30 around x axis:")
-print(rotation2)
-print("Current camera transformation matrix:")
-print(current_camera_transformation)
-
 # Object position in the world space.
 object_position_world = np.array([2, 3, 4], dtype=np.float32)
 
 # TODO: compute the object's position in the camera space using the camera transformation matrix.
-# Go backward through the transformation inverting each of them
+# use the inverse of the camera transformation matrix to transform the object position from the world space to the camera space.
 composite_object_transformation = np.linalg.inv(current_camera_transformation)
 object_position_camera = np.matmul(composite_object_transformation, np.append(object_position_world, 1))
 object_position_camera = object_position_camera[:3]
-# print("Object position in the world space:")
-# print(object_position_world)
+
 print("Object Position in the camera space:")
 print(object_position_camera)
-
-#object_position_camera = None
 
 # TODO: visualize the object's position in the world space and the camera space.
 fig = plt.figure(figsize=(8, 4))
